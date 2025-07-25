91dd6d2bd060f684595f9934705d8ee3110b3f98
rosetta8 <davidjordan_8@hotmail.com>
2025-07-25 17:03:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ import requests
 # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon") 
 
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
         
 
